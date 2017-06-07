871ad8497983e7fbade48b7bd9660d405320e632
stamlercas <stamlercas@yahoo.com>
2017-06-07 17:27:22 -0400

    creating users

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -48,8 +48,10 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|string|max:255',
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
+            'username' => 'require|string|min:6|unique:users|without_spaces',
             'password' => 'required|string|min:6|confirmed',
         ]);
     }
@@ -63,8 +65,10 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'first_name' => $data['first_name'],
+            'last_name' => $data['last_name'],
             'email' => $data['email'],
+            'username' => $data['username'],
             'password' => bcrypt($data['password']),
         ]);
     }
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,6 +6,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="theme-color" content="#E95420" />
 
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
         <!--
         <link rel='stylesheet' href='{{ asset('css/bootstrap.min.css') }}' />
         <link rel='stylesheet' href='{{ asset('css/navbar-fixed-side.css') }}' />
