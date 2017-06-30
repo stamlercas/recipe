bd1b4ef4c9dc99cddf9ac5ce5ec4cefb148f9f3f
stamlercas <stamlercas@yahoo.com>
2017-06-30 10:48:18 -0400

    updated homepage layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,15 +5,21 @@
 @endsection
 
 @section('content')
-	<div class="vertical-center">
-		<h1>Welcome to {{ config('app.name') }}!</h1>
+	<div class="vertical-center" style="width:100%;">
+		
 		<div class="row">
-			<div class="col-md-6">
-				<a href="{{ route('register') }}" class="btn btn-success btn-block" role="button">Register</a>
-	        </div>
-	        <div class="col-md-6">
-	        	<a href="{{ route('login') }}" class="btn btn-primary btn-block" role="button">Login</a>
-	        </div>
+			<div class="col-sm-offset-3 col-sm-6">
+				<div class="row">
+					<h1 class="text-center">Welcome to {{ config('app.name') }}!</h1>
+					<div class="col-sm-6">
+						<a href="{{ route('register') }}" class="btn btn-success btn-block" role="button">Register</a>
+			        </div>
+			        <div class="col-sm-6">
+			        	<a href="{{ route('login') }}" class="btn btn-primary btn-block" role="button">Login</a>
+			        </div>
+				</div>
+			</div>
 		</div>
+		
 	</div>
 @endsection
