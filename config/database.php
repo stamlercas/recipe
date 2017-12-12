d1be13e0e28678ca4da4e577c779d3a345a0b304
unknown <stamlercas@yahoo.com>
2017-12-12 14:31:27 -0500

    fix missing parenthesis

--- config/database.php
+++ config/database.php
@@ -43,7 +43,7 @@ return [
         ],
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', env('CLEAR_DB_HOST'),
+            'host' => env('DB_HOST', env('CLEAR_DB_HOST')),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', env('CLEAR_DB_DATABASE')),
             'username' => env('DB_USERNAME', env('CLEAR_DB_USERNAME')),
