1eddc0454b2e7129c3163597f5a06c5203e82881
unknown <stamlercas@yahoo.com>
2018-01-02 13:12:59 -0500

    change return

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -97,10 +97,10 @@ class RecipeController extends Controller
         }
 
         $url .= "&maxResult=50&start=0";   // bumping results so users have more to see
-
+        
+        $results = json_decode(file_get_contents($url));
         return view('search.results', ['results' => $results, 
             'users_ingredients' => Auth::user()->ingredients()->get()]);
-        $results = json_decode(file_get_contents($url));
 
         foreach($results->matches as $result) {
             $temp = array();
